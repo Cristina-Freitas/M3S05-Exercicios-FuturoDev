fd590f059c4f85212b3c51a6df209285b6950897
CrisTinaFLN <crisjf.tina@hotmail.com>
2023-07-09 15:57:17 -0300

    first commit

---
+++ src/main/java/br/com/despesas/M3S05ExerciciosFuturoDev/M3S05ExerciciosFuturoDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.despesas.M3S05ExerciciosFuturoDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3S05ExerciciosFuturoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3S05ExerciciosFuturoDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/despesas/M3S05ExerciciosFuturoDev/M3S05ExerciciosFuturoDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.despesas.M3S05ExerciciosFuturoDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3S05ExerciciosFuturoDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
